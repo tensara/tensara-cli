8142dc3c994ff298f323b5390af96ff7e773c0b3
Harmya Bhatt <hvbhatt@purdue.edu>
2025-05-21 17:01:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ main() {
   echo "====================="
   
   # Check for version argument
-  VERSION=${1:-"v0.2.0"}
+  VERSION=${1:-"v1.0.0"}
   
   detect_platform
   download_binary "$VERSION"
